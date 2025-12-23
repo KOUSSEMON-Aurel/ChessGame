f0f46b7e1b01cef4eef050cbc6cb3a98fd6b39d9
KOUSSEMON-Aurel <reyseilfullbryger@gmail.com>
2025-12-23 17:42:18 +0000

    Remove Unix-specific logging from iopiper.go for Windows compatibility

--- src/engine/iopiper.go
+++ src/engine/iopiper.go
@@ -10,18 +10,12 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"os"
 	"os/exec"
 )
 
 func main() {
-	f, _ := os.OpenFile("/tmp/iopiper.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if f != nil {
-		log.SetOutput(f)
-	}
-	log.Println("IOPiper started")
 	var clientAddr net.Addr
 
 	// Expect an executable path as 2nd arg
@@ -33,7 +27,6 @@ func main() {
 	// Set up UDP listner
 	pc, err := net.ListenPacket("udp", "0.0.0.0:7070")
 	if err != nil {
-		log.Println("Failed to listen:", err)
 		os.Exit(2)
 	}
 	defer pc.Close()
@@ -66,14 +59,11 @@ func main() {
 	for {
 		n, addr, err := pc.ReadFrom(buffer)
 		if err == nil {
-			log.Printf("Received %d bytes from %v: %s", n, addr, string(buffer[:n]))
 			clientAddr = addr
 			if proc.Process == nil {
 				// Start the subprocess
-				log.Println("Starting subprocess")
 				err := proc.Start()
 				if err != nil {
-					log.Println("Failed to start subprocess:", err)
 					os.Exit(5)
 				}
 			}
